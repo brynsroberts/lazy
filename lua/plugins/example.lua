5969648b11a9a3e42d4d77d3ad8b6c42de083cd9
brynsroberts <bryan.roberts.home@gmail.com>
2024-01-06 12:11:33 -0800

    tokyo night transparent

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -1,6 +1,17 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
+if true then return {
+  {
+  "folke/tokyonight.nvim",
+  opts = {
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+  },
+},
+} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
